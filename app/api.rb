ac2365ae8963840137da1780be23d0a98aaa9b6e
ethan ozelius <e.ozelius@gmail.com>
2018-01-23 23:32:04 -0500

    all endpoints defined in app/api.rb

--- app/api.rb
+++ app/api.rb
@@ -3,6 +3,36 @@ require 'json'
 
 module Todoable
   class API < Sinatra::Base
+    # Retrieves all lists
+    get '/lists' do
+    end
 
+    # Creates a list
+    post '/lists' do
+    end
+
+    # Retrieve the list information
+    get '/lists/:list_id' do
+    end
+
+    # Updates the list
+    patch '/lists/:list_id' do
+    end
+
+    # Deletes the list and all items in it
+    delete '/lists/:list_id' do
+    end
+
+    # Creates a to_do item in this list
+    post '/lists/:list_id/items' do
+    end
+
+    # Mark this to_do item as finished
+    put '/lists/:list_id/items/:item_id/finish' do
+    end
+
+    # Deletes the item
+    delete '/lists/:list_id/items/:item_id' do
+    end
   end
 end
