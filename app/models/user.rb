4a95764d243c4096967940cc70372695b8608da1
ethan ozelius <e.ozelius@gmail.com>
2018-01-28 21:23:11 -0500

    Unit specs for User.rb workign

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,31 @@
-class User < Sequel::Model
-  # SQL relationships
-  one_to_many :lists
+require 'securerandom'
+require_relative '../../config/sequel'
+
+module Todoable
+  class User < Sequel::Model
+    # SQL relationships
+    one_to_many :lists
+
+    # Accessors
+    attr_accessor :email, :password, :password_digest, :token
+
+    # Validations
+    def validate
+      super
+      # email
+      unless @email && /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i =~ @email
+        errors.add(:email, 'invalid email')
+      end
+
+      # password
+      unless @password && @password.length > 6
+        errors.add(:password, 'invalid password')
+      end
+
+      # token
+      unless @token && @token.length > 10
+        errors.add(:token, 'invalid token')
+      end
+    end
+  end
 end
--- spec/unit/app/models/user_spec.rb
+++ spec/unit/app/models/user_spec.rb
@@ -2,11 +2,33 @@ require_relative '../../../../app/models/user'
 
 module Todoable
   RSpec.describe User do
+    let(:token) { SecureRandom.urlsafe_base64(nil, false) }
 
-    it 'should have an email'
+    it 'should have an email' do
+      user = User.new(email: '',
+                      password: 'asdfasdf',
+                      token: token)
+      expect(user.valid?).to eq(false)
+      user.email = 'asdf@asdf.com'
+      expect(user.valid?).to eq(true)
+    end
 
-    it 'should have a password_digest'
+    it 'should have a password_digest' do
+      user = User.new(email: 'asdf@asdf.com',
+                      password: '',
+                      token: token)
+      expect(user.valid?).to eq(false)
+      user.password = 'asdfasdf'
+      expect(user.valid?).to eq(true)
+    end
 
-    it 'should have a token'
+    it 'should have a token' do
+      user = User.new(email: 'asdf@asdf.com',
+                      password: 'asdfasdf',
+                      token: nil)
+      expect(user.valid?).to eq(false)
+      user.token = token
+      expect(user.valid?).to eq(true)
+    end
   end
 end
