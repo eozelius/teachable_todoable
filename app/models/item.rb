40343eb7870189f0ac6cce3abc61f192021e54b3
ethan ozelius <e.ozelius@gmail.com>
2018-01-28 21:48:39 -0500

    Item model unit spec'd

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,25 @@
-class Item < Sequel::Model
-  # SQL relationships
-  many_to_one :list
+require_relative '../../config/sequel'
 
+module Todoable
+  class Item < Sequel::Model
+    # SQL relationships
+    many_to_one :list
 
+    # Attributes
+    attr_accessor :name, :finished_at, :src
+
+    # Validations
+    def validate
+      super
+
+      # name
+      unless @name && @name.length >= 1
+        errors.add(:name, 'invalid name')
+      end
+
+      unless @src
+        errors.add(:src, 'invalid src')
+      end
+    end
+  end
 end
--- spec/unit/app/models/item_spec.rb
+++ spec/unit/app/models/item_spec.rb
@@ -2,10 +2,18 @@ require_relative '../../../../app/models/item'
 
 module Todoable
   RSpec.describe Item do
-    it 'should have a name'
+    it 'should have a name' do
+      item = Item.new(name: nil, finished_at: DateTime.now, src: 'asdfasdf')
+      expect(item.valid?).to eq(false)
+      item.name = 'apples'
+      expect(item.valid?).to eq(true)
+    end
 
-    it "should have 'finished_at' attr"
-
-    it 'should have a src'
+    it 'should have a src' do
+      item = Item.new(name: 'apples', finished_at: DateTime.now, src: nil)
+      expect(item.valid?).to eq(false)
+      item.src = 'asdfasdf'
+      expect(item.valid?).to eq(true)
+    end
   end
 end
