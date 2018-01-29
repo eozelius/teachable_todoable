58e5ac3346990deb23d557d77820481e8ec6465a
ethan ozelius <e.ozelius@gmail.com>
2018-01-28 21:38:20 -0500

    List unit spec'd

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,11 +1,26 @@
-class List < Sequel::Model
-  # SQL relationships
-  many_to_one :user
-  one_to_many :items
+require_relative '../../config/sequel'
 
-  # def validate
-  #   super
-  #   errors.add(:name, "can't be empty") if name.empty?
-  #   errors.add(:written_on, "should be in the past") if written_on >= Time.now
-  # end
+module Todoable
+  class List < Sequel::Model
+    # SQL relationships
+    many_to_one :user
+    one_to_many :items
+
+    # Attributes
+    attr_accessor :name, :src
+
+    # Validations
+    def validate
+      super
+      # name
+      unless @name && @name.length >= 1
+        errors.add(:name, 'invalid name')
+      end
+
+      # src
+      unless @src
+        errors.add(:src, 'invalid src')
+      end
+    end
+  end
 end
--- spec/unit/app/models/list_spec.rb
+++ spec/unit/app/models/list_spec.rb
@@ -1,13 +1,31 @@
 require_relative '../../../../app/models/list'
+require_relative '../../../../app/models/user'
 
 module Todoable
   RSpec.describe List do
-    it 'should have a name'
+    it 'should have a name' do
+      my_list = List.new(name: '', src: 'asdfasdf')
+      expect(my_list.valid?).to eq(false)
+      my_list.name = 'my hobbies'
+      expect(my_list.valid?).to eq(true)
+    end
 
-    it 'should have a src'
+    it 'should have a src' do
+      list = List.new(name: 'my hobbies', src: nil)
+      expect(list.valid?).to eq(false)
+      list.src = 'asdfasdf'
+      expect(list.valid?).to eq(true)
+    end
 
-    it 'should belong to a user'
+    it 'should belong to a user' do
+      list = List.new(name: 'my hobbies', src: nil)
+      expect(list.valid?).to eq(false)
+      list.src = 1
+      expect(list.valid?).to eq(true)
+    end
 
-    it 'should have 0 or more items'
+    it 'should have 0 or more items' do
+      # list.
+    end
   end
 end
