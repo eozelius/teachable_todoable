a69d2dc4f4434392b634157780e69d88f2ed28c3
ethan ozelius <e.ozelius@gmail.com>
2018-02-06 01:05:25 -0500

    Add a spec/helpers directory to hold methods that will be used across the app

--- spec/acceptance/todoable_api_spec.rb
+++ spec/acceptance/todoable_api_spec.rb
@@ -1,18 +1,10 @@
 require 'rack/test'
 require 'json'
 require_relative '../../app/api'
+require_relative '../../spec/helpers/helper_methods'
 
 module Todoable
   RSpec.describe 'Todoable API', :db do
-    include Rack::Test::Methods
-
-    def post_list(list)
-      post '/lists', JSON.generate(list)
-      expect(last_response.status).to eq(201)
-      parsed = JSON.parse(last_response.body)
-      expect(parsed).to match({ 'id' => a_kind_of(Integer) })
-    end
-
     def app
       Todoable::API.new
     end
@@ -20,33 +12,33 @@ module Todoable
     let(:get_lists_response) do
       [
         {
-          "list" => {
-            "id" => 1,
-            "name" => "Urgent Things",
-            "items" => []
+          list: {
+            id: 1,
+            name: 'Urgent Things',
+            items: []
           }
         },
         {
-          "list" => {
-            "id" => 2,
-            "name" => "Medium Priority",
-            "items" => []
+          list: {
+            id: 2,
+            name: 'Medium Priority',
+            items: []
           }
         },
         {
-          "list" => {
-            "id" => 3,
-            "name" => "Low Priority",
-            "items" => []
+          list: {
+            id: 3,
+            name: 'Low Priority',
+            items: []
           }
         }
       ]
     end
 
     it 'records submitted lists and retrieves them' do
-      urgent  = { 'name' => 'Urgent Things' }
-      medium  = { 'name' => 'Medium Priority' }
-      trivial = { 'name' => 'Low Priority' }
+      urgent  = { name: 'Urgent Things' }
+      medium  = { name: 'Medium Priority' }
+      trivial = { name: 'Low Priority' }
 
       post_list(urgent)
       post_list(medium)
@@ -54,8 +46,7 @@ module Todoable
 
       get '/lists'
       expect(last_response.status).to eq(200)
-      response = JSON.parse(last_response.body)
-      expect(response['lists']).to match(get_lists_response)
+      expect(parsed[:lists]).to match(get_lists_response)
     end
   end
 end
