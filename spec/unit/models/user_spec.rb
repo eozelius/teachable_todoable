0c887cc1489cd59f155344bd0c9f6fa916667a4c
ethan ozelius <e.ozelius@gmail.com>
2018-02-12 15:00:30 -0500

    categorizes user unit specs into validations and callbacks

--- spec/unit/models/user_spec.rb
+++ spec/unit/models/user_spec.rb
@@ -2,15 +2,15 @@ require_relative '../../../app/models/user'
 
 module Todoable
   RSpec.describe User do
-    describe 'email' do
-      it 'should be present' do
+    describe 'validates' do
+      it 'email presence' do
         user = User.new(email: '', password: 'asdfasdf')
         expect(user.valid?).to eq(false)
         user.email = 'asdf@asdf.com'
         expect(user.valid?).to eq(true)
       end
 
-      it 'should be unique', :db do
+      it 'email uniqueness', :db do
         asdf_email = 'asdf@asdf.com'
         user_1 = User.create(email: asdf_email, password: 'asdfasdf')
         expect(user_1.valid?).to eq(true)
@@ -21,19 +21,21 @@ module Todoable
         end
       end
 
-      it 'should automatically downcase the email', :db do
+      it 'email downcase-ness', :db do
         user = User.create(email: 'ASDF@ASDF.COM',
                         password: 'asdfasdf')
         expect(user.email).to eq('asdf@asdf.com')
       end
 
-      it 'should have a password_digest' do
+      it 'password_digest presence' do
         user = User.new(email: 'asdf@asdf.com')
         expect(user.valid?).to eq(false)
         user.set(password: 'asdfasdf')
         expect(user.valid?).to eq(true)
       end
+    end
 
+    describe 'callbacks' do
       it 'should generate a token after', :db do
         user = User.new(email: 'asdf@asdf.com',
                         password: 'asdfasdf')
