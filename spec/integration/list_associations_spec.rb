4cd3a22b65441477f85b3a01c721f821a40c79a5
ethan ozelius <e.ozelius@gmail.com>
2018-02-12 17:36:01 -0500

    implements integration test for List to spec SQL relationships with User and Item

---
+++ spec/integration/list_associations_spec.rb
@@ -0,0 +1,43 @@
+require_relative '../../app/models/user'
+require_relative '../../app/models/list'
+
+module Todoable
+  RSpec.describe 'List SQL associations', :db do
+    let(:user) { User.create(email: 'asdf@asdf.com', password: 'asdfasdf') }
+    let(:goals) { List.new(name: 'Life Goals') }
+    let(:yosemite) { Item.new(name: 'Yosemite') }
+    let(:grand_canyon) { Item.new(name: 'Grand Canyon') }
+
+    describe 'association with User' do
+      it 'has a many_to_one relationship with User' do
+        expect(user.lists).to eq([])
+        user.add_list(goals)
+        expect(user.lists).to include(goals)
+        hobbies = List.new(name: 'hobbies')
+        user.add_list(hobbies)
+        expect(user.lists).to include(goals, hobbies)
+      end
+    end
+
+    describe 'association with Item' do
+      before { user.add_list(goals) }
+
+      it 'has a one_to_many relationship with Item' do
+        expect(goals.items).to eq([])
+        goals.add_item(grand_canyon)
+        goals.add_item(yosemite)
+        expect(goals.items).to include(grand_canyon, yosemite)
+      end
+
+      it 'destroys associated items when List is destroyed' do
+        item_count = Item.count
+        goals.add_item(grand_canyon)
+        goals.add_item(yosemite)
+        expect(Item.count).to eq(item_count + 2)
+        goals.destroy
+        expect(Item.count).to eq(item_count)
+        expect(Item.all).not_to include(yosemite)
+      end
+    end
+  end
+end
--- spec/integration/user_associations_spec.rb
+++ spec/integration/user_associations_spec.rb
@@ -5,20 +5,22 @@ module Todoable
   RSpec.describe 'User SQL associations', :db do
     let(:user) { User.create(email: 'asdf@asdf.com', password: 'asdfasdf') }
 
-    it 'has a one_to_many relationship with List' do
-      expect(user.lists).to eq([])
-      grocery_list = List.new(name: 'grocery list')
-      user.add_list(grocery_list)
-      expect(user.lists).to include(grocery_list)
-    end
+    describe 'association with List' do
+      it 'has a one_to_many relationship with List' do
+        expect(user.lists).to eq([])
+        grocery_list = List.new(name: 'grocery list')
+        user.add_list(grocery_list)
+        expect(user.lists).to include(grocery_list)
+      end
 
-    it 'will destroy associated lists when User is destroyed' do
-      list_count = List.count
-      grocery_list = List.new(name: 'grocery list')
-      user.add_list(grocery_list)
-      expect(List.count).to eq(list_count + 1)
-      user.destroy
-      expect(List.count).to eq(list_count)
+      it 'will destroy associated lists when User is destroyed' do
+        list_count = List.count
+        grocery_list = List.new(name: 'grocery list')
+        user.add_list(grocery_list)
+        expect(List.count).to eq(list_count + 1)
+        user.destroy
+        expect(List.count).to eq(list_count)
+      end
     end
   end
 end
