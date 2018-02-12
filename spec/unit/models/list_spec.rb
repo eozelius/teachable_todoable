427329930538f8e84984141c4ca6d38696c803cf
ethan ozelius <e.ozelius@gmail.com>
2018-02-12 16:50:59 -0500

    no change, better categorize list unit specs

--- spec/unit/models/list_spec.rb
+++ spec/unit/models/list_spec.rb
@@ -5,22 +5,24 @@ module Todoable
   RSpec.describe List do
     let(:src) { 'http://todoable.teachable.tech/api/lists/1' }
 
-    it 'should have a name' do
-      my_list = List.new(name: '', user_id: 1)
-      expect(my_list.valid?).to eq(false)
-      my_list.set(name: 'my hobbies')
-      expect(my_list.valid?).to eq(true)
-    end
+    describe 'validates' do
+      it 'name' do
+        my_list = List.new(name: '', user_id: 1)
+        expect(my_list.valid?).to eq(false)
+        my_list.set(name: 'my hobbies')
+        expect(my_list.valid?).to eq(true)
+      end
 
-    it 'should belong to a user' do
-      list = List.new(name: 'my hobbies', user_id: nil)
-      expect(list.valid?).to eq(false)
-      list.user_id = 1
-      expect(list.valid?).to eq(true)
+      it 'should belong to a user' do
+        list = List.new(name: 'my hobbies', user_id: nil)
+        expect(list.valid?).to eq(false)
+        list.user_id = 1
+        expect(list.valid?).to eq(true)
+      end
     end
 
     describe 'callbacks' do
-      it 'should assign its own src attr after_save', :db do
+      it 'should assign a src after_save', :db do
         list = List.create(name: 'my hobbies', user_id: 1)
         expect(list.src).to eq("http://todoable.teachable.tech/api/lists/#{list.id}")
       end
